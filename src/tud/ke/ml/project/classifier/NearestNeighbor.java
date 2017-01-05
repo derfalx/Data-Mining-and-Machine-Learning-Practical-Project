422a2ed4cf131ce46162cf7ec2f21b9157520846
Kristoffer Schneider <kschneider@codingfalx.de>
2017-01-05 19:34:58 +0100

    Implemented basic methods

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -1,6 +1,7 @@
 package tud.ke.ml.project.classifier;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,62 +10,77 @@ import tud.ke.ml.project.util.Pair;
 
 /**
  * This implementation assumes the class attribute is always available (but probably not set).
- * 
  */
 public class NearestNeighbor extends INearestNeighbor implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected double[] scaling;
-	protected double[] translation;
+    private List<List<Object>> model;
 
-	@Override
-	public String getMatrikelNumbers() {
-		return "2879718,2594213,2753711";
-	}
+    protected double[] scaling;
+    protected double[] translation;
 
-	@Override
-	protected void learnModel(List<List<Object>> data) {
-		throw new NotImplementedException();
-	}
+    @Override
+    public String getMatrikelNumbers() {
+        return "2879718,2594213,2753711";
+    }
 
-	@Override
-	protected Map<Object, Double> getUnweightedVotes(List<Pair<List<Object>, Double>> subset) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected void learnModel(List<List<Object>> data) {
+        this.model = data;
+    }
 
-	@Override
-	protected Map<Object, Double> getWeightedVotes(List<Pair<List<Object>, Double>> subset) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected Map<Object, Double> getUnweightedVotes(List<Pair<List<Object>, Double>> subset) {
+        throw new NotImplementedException();
+    }
 
-	@Override
-	protected Object getWinner(Map<Object, Double> votes) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected Map<Object, Double> getWeightedVotes(List<Pair<List<Object>, Double>> subset) {
+        throw new NotImplementedException();
+    }
 
-	@Override
-	protected Object vote(List<Pair<List<Object>, Double>> subset) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected Object getWinner(Map<Object, Double> votes) {
+        double max = Double.MIN_VALUE;
+        for (Double d : votes.values()) {
+            if (max < d)
+                max = d;
+        }
+        for (Object okey : votes.keySet()) {
+            if ( votes.get(okey) == max )
+                return okey;
+        }
+        return null;
+    }
 
-	@Override
-	protected List<Pair<List<Object>, Double>> getNearest(List<Object> data) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected Object vote(List<Pair<List<Object>, Double>> subset) {
+        return this.isInverseWeighting() ? this.getWinner(this.getWeightedVotes(subset)) :
+                this.getWinner(this.getUnweightedVotes(subset));
+    }
 
-	@Override
-	protected double determineManhattanDistance(List<Object> instance1, List<Object> instance2) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected List<Pair<List<Object>, Double>> getNearest(List<Object> data) {
+        ArrayList<Pair<List<Object>, Double>> distances = new ArrayList<>();
+        for (List<Object> instance : this.model) {
+            distances.add(new Pair<>(instance, this.determineManhattanDistance(instance, data)));
+        }
+        return distances;
+    }
 
-	@Override
-	protected double determineEuclideanDistance(List<Object> instance1, List<Object> instance2) {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected double determineManhattanDistance(List<Object> instance1, List<Object> instance2) {
+        throw new NotImplementedException();
+    }
 
-	@Override
-	protected double[][] normalizationScaling() {
-		throw new NotImplementedException();
-	}
+    @Override
+    protected double determineEuclideanDistance(List<Object> instance1, List<Object> instance2) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    protected double[][] normalizationScaling() {
+        throw new NotImplementedException();
+    }
 
 }
