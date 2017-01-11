eb560887671e1889d919e014c70fa34e8af0efc5
Kristoffer Schneider <kschneider@codingfalx.de>
2017-01-11 10:11:15 +0100

    Fixed normalization bug.
    Fixed normalization bug. It seems like we're getting always the same objects, caused by this we've been translating the same data many times.
    All TestCases are now working without errors

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -2,9 +2,7 @@ package tud.ke.ml.project.classifier;
 
 import java.io.Serializable;
 import java.util.*;
-import java.util.stream.Collectors;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import tud.ke.ml.project.util.Pair;
 
 /**
@@ -26,6 +24,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
     @Override
     protected void learnModel(List<List<Object>> data) {
         this.model = data;
+
     }
 
     @Override
@@ -55,7 +54,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
         for (Pair<List<Object>, Double> nearestN : subset) {
             double count = weightedVotes.get(nearestN.getA().get(this.getClassAttribute()));
             //Votes are the sum of the inverted distances
-            count = count + (1/nearestN.getB());
+            count = count + (1 / nearestN.getB());
             weightedVotes.put(nearestN.getA().get(this.getClassAttribute()), count);
         }
         return weightedVotes;
@@ -69,7 +68,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
                 max = d;
         }
         for (Object okey : votes.keySet()) {
-            if ( votes.get(okey) == max )
+            if (votes.get(okey) == max)
                 return okey;
         }
         return null;
@@ -82,31 +81,56 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
     }
 
     @Override
-    protected List<Pair<List<Object>, Double>> getNearest(List<Object> data) {
+    protected List<Pair<List<Object>, Double>> getNearest(List<Object> originalInput) {
         ArrayList<Pair<List<Object>, Double>> distances = new ArrayList<>();
-        if(isNormalizing()){
+
+        List<Object> workingInput = this.copyData(originalInput);
+
+        if (isNormalizing()) {
             double[][] translationScaling = this.normalizationScaling();
             translation = translationScaling[0];
             scaling = translationScaling[1];
-            scaleTranslat(data);
+            scaleTranslate(workingInput);
         }
 
-        for (List<Object> instance : this.model) {
+        List<List<Object>> data = this.deepCopyData(this.model);
+
+        for (List<Object> instance : data) {
             if (isNormalizing()) {
-                scaleTranslat(instance);
+                scaleTranslate(instance);
             }
 
-            if(this.getMetric() == 0)
-                distances.add(new Pair<>(instance, this.determineManhattanDistance(instance, data)));
+            if (this.getMetric() == 0)
+                distances.add(new Pair<>(instance, this.determineManhattanDistance(instance, workingInput)));
             else
-                distances.add(new Pair<>(instance, this.determineEuclideanDistance(instance, data)));
+                distances.add(new Pair<>(instance, this.determineEuclideanDistance(instance, workingInput)));
         }
         distances.sort((x, y) -> (int) (x.getB() * 1000000000000L - y.getB() * 1000000000000L));
         return distances.subList(0, super.getkNearest());
     }
 
-    private void scaleTranslat(List<Object> instance){
-        for(int i = 0; i < instance.size(); i++) {
+    private List<List<Object>> deepCopyData(List<List<Object>> toCopy) {
+        List<List<Object>> data = new ArrayList<>();
+        for (List<Object> innerlist : toCopy) {
+            data.add(this.copyData(innerlist));
+        }
+        return data;
+    }
+
+    private List<Object> copyData(List<Object> toCopy) {
+        List<Object> data = new ArrayList<>();
+        for (Object o : toCopy) {
+            if (o instanceof String) {
+                data.add(new String((String) o));
+            } else if (o instanceof Double) {
+                data.add(new Double((Double) o));
+            }
+        }
+        return data;
+    }
+
+    private void scaleTranslate(List<Object> instance) {
+        for (int i = 0; i < instance.size(); i++) {
             Object o = instance.get(i);
             if (o instanceof Double) {
                 Double d = (Double) o;
@@ -120,19 +144,17 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
     private List<Double> getPlainDistances(List<Object> instance1, List<Object> instance2) {
         List<Double> plainDistances = new ArrayList<>();
 
-        for(int i = 0; i < instance1.size(); i++)
-        {
-            if( i == this.getClassAttribute() )
+        for (int i = 0; i < instance1.size(); i++) {
+            if (i == this.getClassAttribute())
                 continue;
 
             Object attr1 = instance1.get(i);
             Object attr2 = instance2.get(i);
 
-            if( attr1 instanceof String ){
+            if (attr1 instanceof String) {
                 plainDistances.add(attr1.equals(attr2) ? 0d : 1d);
-            }
-            else if( attr1 instanceof Double ){
-                plainDistances.add(Math.abs((Double)attr1 - (Double)attr2));
+            } else if (attr1 instanceof Double) {
+                plainDistances.add(Math.abs((Double) attr1 - (Double) attr2));
             }
         }
 
@@ -141,14 +163,14 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 
     @Override
     protected double determineManhattanDistance(List<Object> instance1, List<Object> instance2) {
-        List<Double> plainDinstances = this.getPlainDistances(instance1,instance2);
+        List<Double> plainDinstances = this.getPlainDistances(instance1, instance2);
         return plainDinstances.stream().mapToDouble(d -> d).sum();
     }
 
     @Override
     protected double determineEuclideanDistance(List<Object> instance1, List<Object> instance2) {
-        List<Double> plainDinstances = this.getPlainDistances(instance1,instance2);
-        return Math.sqrt(plainDinstances.stream().mapToDouble(d -> d*d).sum());
+        List<Double> plainDinstances = this.getPlainDistances(instance1, instance2);
+        return Math.sqrt(plainDinstances.stream().mapToDouble(d -> d * d).sum());
     }
 
     @Override
@@ -156,21 +178,21 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
         double[][] translationScalation = new double[2][this.model.get(1).size()];
         double[][] minMax = new double[2][translationScalation[0].length];
 
-        for(int i = 0; i < minMax[0].length; i++){
+        for (int i = 0; i < minMax[0].length; i++) {
             minMax[0][i] = Double.MAX_VALUE;
             minMax[1][i] = -Double.MAX_VALUE;
         }
 
-        for(List<Object> innerList : model){
+        for (List<Object> innerList : model) {
             int index = 0;
             for (Object o : innerList) {
                 if (o instanceof Double) {
                     double min = minMax[0][index];
                     double max = minMax[1][index];
-                    Double d = (Double)o;
-                    if(d < min)
+                    Double d = (Double) o;
+                    if (d < min)
                         minMax[0][index] = d;
-                    if(d > max )
+                    if (d > max)
                         minMax[1][index] = d;
                 }
                 index++;
@@ -180,8 +202,8 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
         translationScalation[0] = minMax[0];
         for (int i = 0; i < minMax[0].length; i++) {
             translationScalation[1][i] = minMax[1][i] - minMax[0][i];
-            if(translationScalation[1][i] == 0){
-                translationScalation[1][i] = 1;
+            if (translationScalation[1][i] == 0d) {
+                translationScalation[1][i] = 1d;
             }
         }
         return translationScalation;
